d159ebf86662b73c180c5971195ad59720393224
chead-z <155196242+chead-z@users.noreply.github.com>
2024-05-08 16:47:06 -0300

    Create Orion.lua

---
+++ Orion.lua
@@ -0,0 +1,26 @@
+-- Moveable Work At Moblie/PC 
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/thanhdat4461/OrionMoblie/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Shizukii Premium", HidePremium = false, SaveConfig = true, ConfigFolder = "Shizukii Loader"})
+
+local Tab = Window:MakeTab({
+	Name = "Main Scripts ",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Section = Tab:AddSection({
+	Name = "MM2 Scripts"
+})
+Tab:AddButton({
+	Name = "Button!",
+	Callback = function(loadstring(game:HttpGet("https://raw.githubusercontent.com/RedWirePlatinumTwo/RobloxScripts/main/Aimbot.lua"))())
+      		print()
+  	end    
+})
+
+
+
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://11583101826",
+	PremiumOnly = false
+})
