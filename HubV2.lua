5151407230e688cc6bb8e7140f8757e860ea3ec5
chead-z <155196242+chead-z@users.noreply.github.com>
2024-05-29 19:57:01 -0300

    Atualizar o HubV2.lua

--- HubV2.lua
+++ HubV2.lua
@@ -171,10 +171,9 @@ local Paragraph = CreditTab:CreateParagraph({Title = "tskz", Content = "Unknown.
 	Callback = function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/ThatSick/HighlightMM2/main/Main"))()
     end,
- })
 		setclipboard("key: hlontop")
-    end,
  })
+
   
      local Button = HubsTab:CreateButton({
     Name = " Bedol Hub",
