86c8710054f241dd6674f0fb3d871785efc16cf6
lyraEz <155196242+lyraEz@users.noreply.github.com>
2024-07-26 02:06:27 -0300

    Update Shizukii.lua

--- Shizukii.lua
+++ Shizukii.lua
@@ -33,15 +33,42 @@ local Window = Rayfield:CreateWindow({
 warn("Please Support me i put alot of efforts in this.")
 warn("<3")
 
-  local RandomTab = Window:CreateTab("Main Scripts 📜", 4483362458) 
-   local HubsTab = Window:CreateTab("2nd Scripts Tab📜", 4483362458) 
+ 
+ local HubsTab = Window:CreateTab("Murder Mystery 2 ", 4483362458) 
+ local Button = HubsTab:CreateButton({
+	Name = " Symphony Hub| MM2",
+	Callback = function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/ThatSick/ArrayField/main/SymphonyHub.lua'))()
+    end,
+ })
+ local Button = HubsTab:CreateButton({
+	Name = " highlight Hub",
+	Callback = function()
+	Rayfield:Notify({
+   Title = "Key copied from you clipboard",
+   Content = "Have a good time",
+   Duration = 1.5,
+   Image = 4483362458,
+   Actions = { --
+      Ignore = {
+         Name = "Okay!",
+         Callback = function()
+         print("The user tapped Okay!")
+      end
+   },
+},
+})
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/ThatSick/HighlightMM2/main/Main"))()
+    end,
+		setclipboard("hlvipcomingsoon")
+ })
+local Button = HubsTab:CreateButton({
+		Name = " Nexus Hub",
+		Callback = function()
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/s-o-a-b/nexus/main/loadstring"))()
+		end,
+	})
  local ExpTab = Window:CreateTab("Experimental ") 
- local SupportTab = Window:CreateTab("Support Us!", 4483362458) 
- local CreditTab = Window:CreateTab("Credits", 4483362458)
- local ChangeTab = Window:CreateTab("Changelogs", 4483362458) 
-
-
-
 local Paragraph = ExpTab:CreateParagraph({Title = "Dev Note", Content = "Experimental features it can crash your game."})
 local Paragraph = ExpTab:CreateParagraph({Title = "Note", Content = "Hitbox + Esp is universal"})
 
@@ -70,8 +97,13 @@ local Button = ExpTab:CreateButton({
     end,
  })
 
-
-
+ local Button = HubsTab:CreateButton({
+	Name = "Stretch Resolution ",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/ySixxNz/SIXHUB-UNIVERSAL/SixHub-Universal/SixHub-Universal"))()
+    end,
+ })
+ local SupportTab = Window:CreateTab("Support Us!", 4483362458) 
 local Paragraph = CreditTab:CreateParagraph({Title = "Creator", Content = "By chead"})
 
 local Paragraph = SupportTab:CreateParagraph({Title = "Please paste one of these links in you browser", Content = "Follow us on Github or join our server will helps alot!"})
@@ -119,169 +151,24 @@ local Paragraph = SupportTab:CreateParagraph({Title = "Request script ", Content
 		setclipboard("https://discord.gg/yuaN9bG8Mm")
     end,
  })
-
-local Paragraph = CreditTab:CreateParagraph({Title = "Special Thanks to:", Content = ""})
-
-local Paragraph = CreditTab:CreateParagraph({Title = "Cherry", Content = "Testing script!"})
-
-local Paragraph = CreditTab:CreateParagraph({Title = "Ui lib", Content = "Supporting!"})
-
-local Paragraph = CreditTab:CreateParagraph({Title = "Dudyn", Content = "Release part py"})
-
-local Paragraph = CreditTab:CreateParagraph({Title = "tskz", Content = "Unknown..."})
- 
-
-
- local Button = HubsTab:CreateButton({
-	Name = " Symphony Hub| MM2",
-	Callback = function()
-		loadstring(game:HttpGet('https://raw.githubusercontent.com/ThatSick/ArrayField/main/SymphonyHub.lua'))()
-    end,
- })
- local Button = HubsTab:CreateButton({
-	Name = " highlight Hub",
-	Callback = function()
-	Rayfield:Notify({
-   Title = "Key copied from you clipboard",
-   Content = "Have a good time",
-   Duration = 1.5,
-   Image = 4483362458,
-   Actions = { --
-      Ignore = {
-         Name = "Okay!",
-         Callback = function()
-         print("The user tapped Okay!")
-      end
-   },
-},
-})
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/ThatSick/HighlightMM2/main/Main"))()
-    end,
-		setclipboard("hlvipcomingsoon")
- })
-
+ local ChangeTab = Window:CreateTab("Blade ball ", 4483362458) 
      local Button = HubsTab:CreateButton({
     Name = " Bedol Hub | key",
     Callback = function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/nqxlOfc/Loaders/main/Blade_Ball.lua'))() -- The function that takes place when the button is pressed
     end,
  })
-     local Button = HubsTab:CreateButton({
-    Name = " Dark Cheat | MM2",
-    Callback = function()
-    loadstring(game:HttpGet("https://pastebin.com/raw/ECCeYtXA"))() -- The function that takes place when the button is pressed
-    end,
- })
-local Button = HubsTab:CreateButton({
-	Name = " Da Hood Hub's",
-	Callback = function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/svan1lol/svanhub/main/svanhubdahood.lua", true))()
-    end,
- })
-
-
+    
  local Button = RandomTab:CreateButton({
-	Name = " Redz Hub | blox fruits ",
+	Name = " Redz Hub",
 	Callback = function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()
-    end,
- })
- local Button = RandomTab:CreateButton({
-	Name = " Luarmor Hub",
-	Callback = function()
-		loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
-    end,
- })
- local Button = RandomTab:CreateButton({
-	Name = " Miojo Hub (key)",
-	Callback = function()
-		loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/sumidassz/miojoHub/main/hub.lua", true))()
-    end,
- })
- local Button = RandomTab:CreateButton({
-	Name = " Vecto Hub (key)",
-	Callback = function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/AAwful/VectorHub/main/Loader.lua"))()
-    end,
- })
- local Button = RandomTab:CreateButton({
-	Name = " Yutv Hub (key)",
-	Callback = function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/AAwful/VectorHub/main/Loader.lua"))()
-    end,
- })
- local Button = RandomTab:CreateButton({
-	Name = " Ry's Evade Gui",
-	Callback = function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/RyAtlasX/Script-Storages/main/Evade%20Gui.lua"))()
-    end,
- })
- local Button = RandomTab:CreateButton({
-	Name = " Car Dealership Tycoon",
-	Callback = function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Marco8642/science/main/car%20dealer", true))()
-    end,
- })
- local Button = RandomTab:CreateButton({
-	Name = " W-azure Hub",
-	Callback = function()
-		loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
-    end,
- })
- local Button = RandomTab:CreateButton({
-	Name = " Break In Story | OP",
-	Callback = function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/RyAtlasX/Script-Storages/main/Break%20In%20Story.lua"))()
-    end,
- })
- local Button = RandomTab:CreateButton({
-	Name = " Muscle Legends | V.G",
-	Callback = function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/RyAtlasX/Atlas_hub/main/Muscle-Legends.txt"))()
-    end,
- })
- local Button = RandomTab:CreateButton({
-	Name = " VAPE v4 | Bedwars",
-	Callback = function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/RyAtlasX/Script-Storages/main/VAPE%20V4%20%5Bbedwar%5D"))()
-    end,
- })
- local Button = RandomTab:CreateButton({
-	Name = " Arsenal Scripts | 2 Hubs included",
-	Callback = function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/RyAtlasX/test/main/Arsenal%20scripts1"))()
-    end,
- })
- local Button = RandomTab:CreateButton({
-	Name = " Vynixu Scripts | Doors",
-	Callback = function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/RyAtlasX/test/main/Doors2"))()
-    end,
- })
- 
- local Button = HubsTab:CreateButton({
-	Name = " Stretch Resolution",
-	Callback = function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/ySixxNz/SIXHUB-UNIVERSAL/SixHub-Universal/SixHub-Universal"))()
-    end,
- })
-
- local Button = HubsTab:CreateButton({
-	Name = " Ez Hub",
-	Callback = function()
-		loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Ez%20Hub.txt"))()
     end,
  })
 local Button = HubsTab:CreateButton({
 		Name = " Visual Hub",
 		Callback = function()
 			loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/0aefed07fd25b2369e282001ca3c2838.lua"))()
-		end,
-	})
-local Button = HubsTab:CreateButton({
-		Name = " Nexus Hub",
-		Callback = function()
-			loadstring(game:HttpGet("https://raw.githubusercontent.com/s-o-a-b/nexus/main/loadstring"))()
 		end,
 	})
 
