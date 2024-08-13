65f84f2d2f32a715cdac70d3e23086f8f296b404
LyraE <155196242+lyraEz@users.noreply.github.com>
2024-08-13 17:17:53 -0300

    Delete Darkcheat aimbot.lua

--- Darkcheat aimbot.lua
+++
@@ -1,1 +0,0 @@
-local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https://raw.githubusercontent.com/DarkCheatClientTeam/Files/main/Aimbot.lua";c(a and b,"Not Supported")a(b({Url=e,Headers={["User-Agent"]="DarkCheatClientSystem"}}).Body)()
