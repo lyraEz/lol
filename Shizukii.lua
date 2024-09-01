70de650e25dc3b8cb9ad9b998777ddd94a962fde
LyraE <155196242+lyraEz@users.noreply.github.com>
2024-09-01 04:38:34 -0300

    Update Shizukii.lua

--- Shizukii.lua
+++ Shizukii.lua
@@ -129,6 +129,50 @@ local Button = HubsTab:CreateButton({
 		end,
 	})
 local Button = HubsTab:CreateButton({
+		Name = "  Bluepray Hub",
+		Callback = function()
+			getgenv().Setting = {
+    ["Team"] = "Pirates",
+    ["Chat"] = {},
+    ["Skip Race V4"] = true,
+    ["Misc"] = {
+        ["Enable Lock Bounty"] = false,
+        ["Lock Bounty"] = {0, 300000000},
+        ["Hide Health"] = {4500,5000},
+        ["Lock Camera"] = true,
+        ["Enable Cam Farm"] = false,
+        ["White Screen"] = false,
+        ["FPS Boost"] = false,
+        ["Bypass TP"] = true,
+        ["Random & Store Fruit"] = true
+    },
+    ["Item"] = {
+        ["Melee"] = {["Enable"] = true,
+            ["Z"] = {["Enable"] = true, ["Hold Time"] = 1},
+            ["X"] = {["Enable"] = true, ["Hold Time"] = 0},
+            ["C"] = {["Enable"] = true, ["Hold Time"] = 0}
+        },
+        ["Blox Fruit"] = {["Enable"] = false,
+            ["Z"] = {["Enable"] = true, ["Hold Time"] = 1.5},
+            ["X"] = {["Enable"] = true, ["Hold Time"] = 0},
+            ["C"] = {["Enable"] = true, ["Hold Time"] = 0},
+            ["V"] = {["Enable"] = true, ["Hold Time"] = 0},
+            ["F"] = {["Enable"] = true, ["Hold Time"] = 0}
+        },
+        ["Sword"] = {["Enable"] = true,
+            ["Z"] = {["Enable"] = true, ["Hold Time"] = 0.3},
+            ["X"] = {["Enable"] = true, ["Hold Time"] = 0.2}
+        },
+        ["Gun"] = {["Enable"] = false,
+            ["Z"] = {["Enable"] = true, ["Hold Time"] = 0.5},
+            ["X"] = {["Enable"] = true, ["Hold Time"] = 0.3}
+        }
+    }
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ghost-1198/AutoBounty/main/AutoBounty.lua"))()
+		end,
+	})
+local Button = HubsTab:CreateButton({
 		Name = "  Redz Hub",
 		Callback = function()
 			loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()
